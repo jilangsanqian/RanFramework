ec87acb36fa0c990b20784af5e6cb4d4f46c79c6
ranhai <jilangsanqian@163.com>
2017-04-23 18:29:02 +0800

    add all

---
+++ App/Common/Conf.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Conf
+ *
+ * @author ranhai
+ */
+class Conf {
+    //put your code here
+}
